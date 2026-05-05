7c90117f80b3d8568078a6084dff5400c20dbf1f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-05 12:18:44 +0400

    BorrowItem() method is written

--- src/Services/BorrowingServices.java
+++ src/Services/BorrowingServices.java
@@ -1,8 +1,11 @@
 package Services;
 
+import Entities.LibraryItem;
+
 import java.util.Scanner;
 
 public class BorrowingServices {
+    LibraryItem libraryItem = new LibraryItem();
     LibraryItemServices libraryItemServices = new LibraryItemServices();
     MemberServices memberServices = new MemberServices();
     Scanner scanner = new Scanner(System.in);
@@ -12,7 +15,18 @@ public class BorrowingServices {
         String borrowerID = scanner.nextLine();
             if (memberServices.FindMemberByID() == true){
                 System.out.println(borrowerID + " is MEMBER");
+                System.out.println("Please enter the item ID you want to borrow");
+                    String itemToBorrow = scanner.nextLine();
+                    if (libraryItemServices.FindItemById() == true){
+                        System.out.println(itemToBorrow+" "+ "is available");
+                        libraryItem.setStatus("Borrowed");
+                        System.out.println("Borrowed Done");
+                    }else {
+                        System.out.println("SORRY, Item is not available");
+                    }
 
+            }else {
+                System.out.println( borrowerID +" "+ "Is NOT MEMBER");
             }
     }
 }
